a157d37ad5d3492f89ff9240e40169242919a56d
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2023-12-18 21:59:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function firstWord(s) {
-  // your code here
+	 const words = s.split(' ');
+
+  // Return the first word or the entire string if there are no spaces
+  return words.length > 0 ? words[0] : s;
 }
 
 // Do not change the code below
